c67a1b71369ccc87370040ba68d6ad7482ea7e72
Emanuelly Carvalho <emanuellycarvalho815@gmail.com>
2026-05-07 09:55:15 -0300

    Subindo projeto

---
+++ apps/clientes/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from .forms import Clienteform
+from .models import Cliente
+
+# Create your views here.
+
+def novo_cliente(request):
+    clientes = Cliente.objects.all()
+    template_name = 'novo_Cliente.html'
+    context = {}
+    if request.method == 'POST':
+        form = Clienteform(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('novo_cliente')
+        else:
+            return HttpResponse("<h1>Deu erro no teu formulário<h1>")
+        
+    Form = Clienteform()
+    context['form'] = Form
+    context['clientes'] = clientes
+    return render(request, template_name, context)
+
+def atualizar_cliente(request, id):
+    try:
+        cliente = Cliente.objects.get(id=id)
+    except Cliente.DoesNotExist:
+        return HttpResponse('<h1>Cliente não encontrado/h1')
+    
+    if request.method == 'POST':
+        form = Clienteform(request.POST, instance=cliente)
+        if form.is_valid():
+            form.save()
+            return redirect('novo_cliente')
+        else:
+            return HttpResponse('<h1>Erro na atualização do cliente<h1>')
+    
+    form = Clienteform(instance=cliente)
+    template_name = 'novo_cliente.html'
+    clientes = Cliente.objects.all()
+    context ={
+        'form': form,
+        'clientes': clientes
+    }
+    return render(request, template_name, context) 
+
+def excluir_cliente(request, id):
+    try:
+        cliente = Cliente.objects.get(id=id)
+        cliente.delete()
+    except Cliente.DoesNotExist:
+        return HttpResponse('<h1>Erro ao excluir o cliente. Não encontrado</h1>')
+    return redirect('novo_cliente')
